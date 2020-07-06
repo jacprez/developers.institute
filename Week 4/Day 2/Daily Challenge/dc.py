108401e364c5792a3d118f970b5c93fab6a77293
jacprez <jprezant1@gmail.com>
2020-07-06 17:42:18 +0300

    daily challenge part 1

---
+++ Week 4/Day 2/Daily Challenge/dc.py
@@ -0,0 +1,14 @@
+user_birthday = input("Enter your age: ")
+i_times = int(user_birthday[-1])
+number_of_i = "i" * i_times
+print(f"""
+      ___{number_of_i}___
+      |:H:a:p:p:y:|
+    __|___________|__
+   |^^^^^^^^^^^^^^^^^|
+   |:B:i:r:t:h:d:a:y:|
+   |                 |
+   ~~~~~~~~~~~~~~~~~~~ """)
+# for number in user_birthday:
+#     i = int(number[-1])
+#     print(i)
--- Week 4/Day 2/Exercise XP Gold/xpgold.py
+++ Week 4/Day 2/Exercise XP Gold/xpgold.py
@@ -1,16 +1,16 @@
 # Exercise 1 
 
 # Take three numbers from the user and print the greatest number.
-number1 = float(input("Enter your first number: "))
-number2 = float(input("Enter your second number: "))
-number3 = float(input("Enter your third number: "))
-if (number1 > number2) and (number1 > number3):
-   largest = number1
-elif (number2 > number1) and (number2 > number3):
-   largest = number2
-else:
-   largest = number3
-print("The largest number is",largest)
+# number1 = float(input("Enter your first number: "))
+# number2 = float(input("Enter your second number: "))
+# number3 = float(input("Enter your third number: "))
+# if (number1 > number2) and (number1 > number3):
+#    largest = number1
+# elif (number2 > number1) and (number2 > number3):
+#    largest = number2
+# else:
+#    largest = number3
+# print("The largest number is",largest)
 
 
 # Exercise 2 
@@ -35,3 +35,24 @@ list1 = [1, 2, 3, 4]
 list2 = [1, 2, 3, 4]
 list1.extend(list2)
 print(list1)
+
+# Exercise 7
+
+import random
+random_number = random.randint(1, 9)
+print(random_number)
+user_guesses = int(input("Guess a number from 1-9: "))
+while user_guesses != random_number:
+   user_guesses = int(input("Wrong number. Guess another number from 1-9: "))
+print("Nice you got it!")
+
+# Exercise 9
+
+numbers= range(1,1000001)
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+
+
+
